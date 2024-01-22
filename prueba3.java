e216a1866cef819bb5e4bd2aa9a6671be2771386
Vapemo <vale_perez@ciencias.unam.mx>
2024-01-22 01:39:41 -0600

    nuevo

---
+++ prueba3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * Clase para representar el juego.Representación del juego computadora contra usuaario.
+ *@author Valeria Pérez
+ * @version 9 abril 2022
+ */
+public class  prueba3{
+ static int opcion=-1;
+Libro lib [] = new Libro[5];
+
+public  Libro[])llamada{
+
+ lib [0] = new Libro("Introduccion al desarrollo de programas con Java"," Amparo Lopez Gaona","Facultad de Ciencias, UNAM",1090);
+ lib [1] = new Libro ("Introduccion al desarrollo de programas con Java"," Amparo Lopez Gaona","Facultad de Ciencias, UNAM",1090);
+ return lib;
+
+ //for(int i =0;i<lib.length;i++){
+ //System.out.println(lib[i].toString());
+ //}
+}
+}
